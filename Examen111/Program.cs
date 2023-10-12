3b743e6c83d286e7563c2191efe8b9c727a5f923
acurling <acurling@hotmail.com>
2023-10-11 18:44:48 -0600

    actualizacion

--- Examen111/Program.cs
+++ Examen111/Program.cs
@@ -11,6 +11,7 @@ namespace Examen111
         static void Main(string[] args)
         {
             Console.WriteLine(  "hola mundo");
+            Console.WriteLine("Buenas noches");
             Console.ReadLine();
         }
         
